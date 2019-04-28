018f7d2ffbd5941b099f581f162f1d336ca2f4ea
Kavelaa <37141924+Kavelaa@users.noreply.github.com>
2019-04-28 12:36:29 +0800

    Cancel not pretty sequential display, now please use prop "showDots" to open this function. Once you open, user can click the dot to switch to corresponding pic directly.

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -9,7 +9,7 @@ import styles from './banner.module.scss'
 /*srcArr Array 存放图片地址的数组，以数组顺序作为轮播顺序
   srcArr子元素 String/Object 如需同时将图片作为超链接，以对象{src: String, url: String} (url为希望跳转的网页链接)传入*/
 //showProgress Bool 是否显示进度条，显示则使用ProgressBar组件的回调属性，不显示则内建定时器执行轮播
-//showIndex Bool 是否显示当前轮播序号，默认显示
+//showDots Bool 是否显示当前轮播序号，显示则用户可以通过点击Dot进行更高效的轮播图切换
 
 class Banner extends React.Component {
   constructor(props) {
@@ -18,7 +18,7 @@ class Banner extends React.Component {
       order: 0,
       in: true,
       switch: true,
-      appear: false
+      arrowAppear: false
     }
 
     this.bannerRef = null
@@ -55,6 +55,43 @@ class Banner extends React.Component {
         }
       })
     }
+    this.goTo = idx => {
+      this.setState({
+        order: idx,
+        in: true
+      })
+    }
+
+    //高阶函数，对不同的操作，封装一些重复性的逻辑，action Function，idx Number（特别用于goTo操作的参数）
+    this.wrapperAction = (action, idx) => {
+      if (this.timer) {
+        clearInterval(this.timer)
+      }
+      this.setState(
+        {
+          in: false,
+          switch: false
+        },
+        () => {
+          setTimeout(() => {
+            if (!this.props.showProgress) {
+              this.timer = setInterval(this.change, this.props.duration * 1000)
+            }
+            //操作由此开始
+            if (!isNaN(idx)) {
+              action(idx)
+            } else {
+              action()
+            }
+            //到此为止，其余皆是重复性逻辑
+            this.setState({
+              switch: true
+            })
+          }, 205)
+        }
+      )
+    }
+
     this.change = () => {
       this.setState(
         {
@@ -63,62 +100,18 @@ class Banner extends React.Component {
         () => {
           setTimeout(() => {
             this.goAhead()
-          }, 410)
-        }
-      )
-    }
-    this.last = () => {
-      if (this.timer) {
-        clearInterval(this.timer)
-      }
-      this.setState(
-        {
-          in: false,
-          switch: false
-        },
-        () => {
-          setTimeout(() => {
-            if (!this.props.showProgress) {
-              this.timer = setInterval(this.change, this.props.duration * 1000)
-            }
-            this.goBack()
-            this.setState({
-              switch: true
-            })
-          }, 405)
-        }
-      )
-    }
-    this.next = () => {
-      if (this.timer) {
-        clearInterval(this.timer)
-      }
-      this.setState(
-        {
-          in: false,
-          switch: false
-        },
-        () => {
-          setTimeout(() => {
-            if (!this.props.showProgress) {
-              this.timer = setInterval(this.change, this.props.duration * 1000)
-            }
-            this.goAhead()
-            this.setState({
-              switch: true
-            })
-          }, 405)
+          }, 210)
         }
       )
     }
     this.appear = () => {
       this.setState({
-        appear: true
+        arrowAppear: true
       })
     }
     this.disappear = () => {
       this.setState({
-        appear: false
+        arrowAppear: false
       })
     }
   }
@@ -132,8 +125,19 @@ class Banner extends React.Component {
   render() {
     let srcArr = this.props.srcArr
     let showProgress = this.props.showProgress
-    let showIndex = this.props.showIndex
+    let showDots = this.props.showDots
     let arrow = ['<', '>']
+    let dots = srcArr.map((val, idx) => (
+      <div
+        key={idx}
+        className={(
+          styles.dots +
+          ' ' +
+          (this.state.order === idx ? styles['dots-selected'] : '')
+        ).trim()}
+        onClick={() => this.wrapperAction(this.goTo, idx)}
+      />
+    ))
     let bannerWidth = this.bannerRef ? this.bannerRef.offsetWidth : 0
 
     return (
@@ -143,29 +147,41 @@ class Banner extends React.Component {
         onMouseLeave={this.disappear}
         className={(this.props.userClass + ' ' + styles.default).trim()}
       >
-        {this.state.appear && (
+        {this.state.arrowAppear && (
           <div className={styles.toggle}>
-            <button onClick={this.last}>{arrow[0]}</button>
-            <button onClick={this.next}>{arrow[1]}</button>
+            <button onClick={() => this.wrapperAction(this.goBack)}>
+              {arrow[0]}
+            </button>
+            <button onClick={() => this.wrapperAction(this.goAhead)}>
+              {arrow[1]}
+            </button>
           </div>
         )}
-        {showIndex && (
-          <div className={styles.index}>
-            {this.state.order + 1 + '/' + srcArr.length}
-          </div>
-        )}
+        {showDots && <div className={styles.index}>{dots}</div>}
         <div className={styles.wrapper}>
-          <a className={styles.href} href={typeof srcArr[this.state.order] === "object" ? srcArr[this.state.order].url : null}>
+          <a
+            className={styles.href}
+            href={
+              typeof srcArr[this.state.order] === 'object'
+                ? srcArr[this.state.order].url
+                : null
+            }
+            target="_blank"
+          >
             <Transition
               in={this.state.in}
-              timeout={400}
+              timeout={200}
               appear
               mountOnEnter
               unmountOnExit
             >
               {state => (
                 <img
-                  src={typeof srcArr[this.state.order] === "object" ? srcArr[this.state.order]['src'] : srcArr[this.state.order]}
+                  src={
+                    typeof srcArr[this.state.order] === 'object'
+                      ? srcArr[this.state.order]['src']
+                      : srcArr[this.state.order]
+                  }
                   alt={'banner'}
                   className={styles.img + ' ' + styles['img-' + state]}
                 />
@@ -189,14 +205,14 @@ class Banner extends React.Component {
 
 Banner.propTypes = {
   userClass: PropTypes.string,
-  showIndex: PropTypes.bool,
+  showDots: PropTypes.bool,
   showProgress: PropTypes.bool,
   srcArr: PropTypes.array.isRequired,
   duration: PropTypes.number.isRequired
 }
 Banner.defaultProps = {
   userClass: '',
-  showIndex: false,
+  showDots: false,
   showProgress: false
 }
 
